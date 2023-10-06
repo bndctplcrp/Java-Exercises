020b4cd6c01a04f5e11da320525d09d47d60a27f
bndctplcrp <benedictpolicarpio08@gmail.com>
2023-10-06 22:42:50 +0800

    Added java exercises

---
+++ Java1_IntegerToBinary.java
@@ -0,0 +1,11 @@
+public class Java1_IntegerToBinary{
+    public static void main(String[] args) {
+        
+        int integer = 56;
+        String binaryString = Integer.toBinaryString(integer);
+
+        System.out.println("Convert Decimal number "+ integer +" to Binary Number is  "+binaryString);
+    
+    }
+
+}
